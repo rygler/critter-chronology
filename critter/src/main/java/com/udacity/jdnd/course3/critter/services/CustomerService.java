73c4981b6a352c8ee1a7c5e6698a93e2918b00f1
Raphael G <raphaelgal@gmail.com>
2022-02-24 15:28:20 +0200

    Removed unused repository

--- critter/src/main/java/com/udacity/jdnd/course3/critter/services/CustomerService.java
+++ critter/src/main/java/com/udacity/jdnd/course3/critter/services/CustomerService.java
@@ -1,7 +1,6 @@
 package com.udacity.jdnd.course3.critter.services;
 
 import com.udacity.jdnd.course3.critter.repositories.CustomerRepository;
-import com.udacity.jdnd.course3.critter.repositories.CustomerRepositoryV2;
 import com.udacity.jdnd.course3.critter.user.Customer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,25 +13,21 @@ import java.util.Optional;
 public class CustomerService {
     @Autowired
     CustomerRepository customerRepository;
-    @Autowired
-    CustomerRepositoryV2 customerRepositoryV2;
 
     public Customer save(Customer customer) {
-        return customerRepositoryV2.save(customer);
+        return customerRepository.save(customer);
     }
 
     public List<Customer> findAll() {
-        return customerRepositoryV2.findAll();
+        return customerRepository.findAll();
     }
 
     public Customer find(long customerId) {
-        Optional<Customer> customerOptional = customerRepositoryV2.findById(customerId);
+        Optional<Customer> customerOptional = customerRepository.findById(customerId);
         if (customerOptional.isPresent()) {
             return customerOptional.get();
         } else {
             throw new EntityNotFoundException("Customer with id: " + customerId + " not found. Please verify that id is correct.");
         }
     }
-
-
 }
