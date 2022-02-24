cacc692ba362f93de6aa5ccd3bf103d9f765a622
Raphael G <raphaelgal@gmail.com>
2022-02-24 15:28:20 +0200

    Removed unused repository

--- critter/src/main/java/com/udacity/jdnd/course3/critter/services/PetService.java
+++ critter/src/main/java/com/udacity/jdnd/course3/critter/services/PetService.java
@@ -2,17 +2,11 @@ package com.udacity.jdnd.course3.critter.services;
 
 import com.udacity.jdnd.course3.critter.pet.Pet;
 import com.udacity.jdnd.course3.critter.repositories.CustomerRepository;
-import com.udacity.jdnd.course3.critter.repositories.CustomerRepositoryV2;
 import com.udacity.jdnd.course3.critter.repositories.PetRepository;
-import com.udacity.jdnd.course3.critter.user.Customer;
-import com.udacity.jdnd.course3.critter.user.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.HttpStatusCodeException;
 
 import javax.persistence.EntityNotFoundException;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -22,7 +16,7 @@ public class PetService {
     PetRepository petRepository;
 
     @Autowired
-    CustomerRepositoryV2 customerRepository;
+    CustomerRepository customerRepository;
 
     public Pet save(Pet pet) {
         Pet persistedPet = petRepository.save(pet);
@@ -48,7 +42,6 @@ public class PetService {
             throw new EntityNotFoundException("Pet with id: " + petId + " not found. Please verify that id is correct.");
         }
     }
-
 
     public List<Pet> findAll() {
         return petRepository.findAll();
